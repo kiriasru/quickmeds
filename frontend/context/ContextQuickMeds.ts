71efecaf092ac0aeb4e265206833ee3d0a7c97ef
kiriasru <kiriasru@gmail.com>
2026-03-17 17:40:37 -0600

    Cambios en ContextQuickMeds

--- frontend/context/ContextQuickMeds.ts
+++ frontend/context/ContextQuickMeds.ts
@@ -1,22 +1,11 @@
 import { createContext } from "react";
-import {
-    CondicionCronica,
-    CondicionCronicaNueva,
-} from "../modelos/CondicionCronica";
-import {
-    ContactoEmergencia,
-    ContactoEmergenciaNuevo,
-} from "../modelos/ContactoEmergencia";
+import { CondicionCronica, CondicionCronicaNueva } from "../modelos/CondicionCronica";
+import { ContactoEmergencia, ContactoEmergenciaNuevo } from "../modelos/ContactoEmergencia";
 import { Dosis, EstadoDosis } from "../modelos/Dosis";
 import { Medicamento, MedicamentoNuevo } from "../modelos/Medicamento";
 import { PantallaApp } from "../modelos/Navegacion";
 import { Recordatorio, RecordatorioNuevo } from "../modelos/Recordatorio";
-import {
-    SesionUsuario,
-    Usuario,
-    UsuarioLogin,
-    UsuarioRegistro,
-} from "../modelos/Usuario";
+import { SesionUsuario, Usuario, UsuarioLogin, UsuarioRegistro } from "../modelos/Usuario";
 
 export const contextQuickMeds = createContext({
     cargandoSesion: false,
@@ -31,39 +20,31 @@ export const contextQuickMeds = createContext({
     condiciones: [] as CondicionCronica[],
     pantallaActual: "HomePublico" as PantallaApp,
 
-    cambiarPantalla: (_pantalla: PantallaApp) => {},
-    registrar: async (_usuario: UsuarioRegistro) => false,
-    iniciarSesion: async (_credenciales: UsuarioLogin) => false,
+    cambiarPantalla: (pantalla: PantallaApp) => {},
+    registrar: async (usuario: UsuarioRegistro) => false,
+    iniciarSesion: async (credenciales: UsuarioLogin) => false,
     cerrarSesion: async () => {},
 
     obtenerMedicamentos: async () => {},
-    agregarMedicamento: async (_medicamento: MedicamentoNuevo) => false,
-    actualizarMedicamento: async (
-        _id: number,
-        _medicamento: Omit<MedicamentoNuevo, "id_usuario">,
-    ) => false,
-    eliminarMedicamento: async (_id: number) => false,
-    cambiarEstadoMedicamento: async (_id: number, _activo: boolean) => false,
+    agregarMedicamento: async (medicamento: MedicamentoNuevo) => false,
+    actualizarMedicamento: async (id: number, medicamento: Omit<MedicamentoNuevo, "id_usuario">) => false,
+    eliminarMedicamento: async (id: number) => false,
+    cambiarEstadoMedicamento: async (id: number, activo: boolean) => false,
 
     obtenerRecordatorios: async () => {},
-    agregarRecordatorio: async (_recordatorio: RecordatorioNuevo) => false,
-    eliminarRecordatorio: async (_id: number) => false,
+    agregarRecordatorio: async (recordatorio: RecordatorioNuevo) => false,
+    eliminarRecordatorio: async (id: number) => false,
 
     obtenerDosis: async () => {},
-    registrarDosis: async (
-        _idMedicamento: number,
-        _estado: EstadoDosis,
-        _fechaHoraProgramada: string,
-    ) => false,
-    actualizarEstadoDosis: async (_idDosis: number, _estado: EstadoDosis) =>
-        false,
+    registrarDosis: async (idMedicamento: number, estado: EstadoDosis, fechaHoraProgramada: string) => false,
+    actualizarEstadoDosis: async (idDosis: number, estado: EstadoDosis) => false,
 
     obtenerPerfil: async () => null as Usuario | null,
     obtenerContactos: async () => {},
-    agregarContacto: async (_contacto: ContactoEmergenciaNuevo) => false,
-    eliminarContacto: async (_id: number) => false,
+    agregarContacto: async (contacto: ContactoEmergenciaNuevo) => false,
+    eliminarContacto: async (id: number) => false,
 
     obtenerCondiciones: async () => {},
-    agregarCondicion: async (_condicion: CondicionCronicaNueva) => false,
-    eliminarCondicion: async (_id: number) => false,
+    agregarCondicion: async (condicion: CondicionCronicaNueva) => false,
+    eliminarCondicion: async (id: number) => false,
 });
